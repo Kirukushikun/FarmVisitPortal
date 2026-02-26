a893f47b00dfc0f3878631752fca4f338dd249f8
raniel-roque <raniel.roque0509@gmail.com>
2026-02-26 09:52:52 +0800

    Added Location management and permits migration

--- database/migrations/2026_02_26_095000_create_permits_table.php
+++ database/migrations/2026_02_26_095000_create_permits_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('forms', function (Blueprint $table) {
+        Schema::create('permits', function (Blueprint $table) {
             $table->id();
 
             $table->string('area');
@@ -33,6 +33,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('forms');
+        Schema::dropIfExists('permits');
     }
 };
