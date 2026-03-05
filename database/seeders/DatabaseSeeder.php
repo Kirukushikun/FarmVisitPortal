bafdb0e30e304dbd6c8d4c31cf9cbb894bb3651b
raniel-roque <raniel.roque0509@gmail.com>
2026-03-05 09:20:43 +0800

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,39 +19,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(11)->create();
 
-        User::factory()->create([
-            'first_name' => 'Jeff',
-            'last_name' => 'Montiano',
-            'username' => 'JMontiano',
-            'user_type' => '1',
-        ]);
+        User::query()->updateOrCreate(
+            ['username' => 'JMontiano'],
+            ['first_name' => 'Jeff', 'last_name' => 'Montiano', 'user_type' => '1']
+        );
 
-        User::factory()->create([
-            'first_name' => 'Adam',
-            'last_name' => 'Trinidad',
-            'username' => 'ATrinidad',
-            'user_type' => '1',
-        ]);
+        User::query()->updateOrCreate(
+            ['username' => 'ATrinidad'],
+            ['first_name' => 'Adam', 'last_name' => 'Trinidad', 'user_type' => '1']
+        );
 
-        User::factory()->create([
-            'first_name' => 'Iverson',
-            'last_name' => 'Guno',
-            'username' => 'IGuno',
-            'user_type' => '1',
-        ]);
+        User::query()->updateOrCreate(
+            ['username' => 'IGuno'],
+            ['first_name' => 'Iverson', 'last_name' => 'Guno', 'user_type' => '1']
+        );
 
-        User::factory()->create([
-            'first_name' => 'Raniel',
-            'last_name' => 'Roque',
-            'username' => 'RRoque',
-            'user_type' => '1',
-        ]);
+        User::query()->updateOrCreate(
+            ['username' => 'RRoque'],
+            ['first_name' => 'Raniel', 'last_name' => 'Roque', 'user_type' => '1']
+        );
 
-        User::factory()->create([
-            'first_name' => 'Jenny',
-            'last_name' => 'Santos',
-            'username' => 'JSantos',
-            'user_type' => '1',
-        ]);
+        User::query()->updateOrCreate(
+            ['username' => 'JSantos'],
+            ['first_name' => 'Jenny', 'last_name' => 'Santos', 'user_type' => '1']
+        );
     }
 }
